e7742d6600211d2b67a069bebfb40b7c595e24b9
556 <68994528+556-mizusawa@users.noreply.github.com>
2020-12-24 10:35:06 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 function App() {
+  const inputbutton = (): void => console.log('hello');
+
   return (
     <div>
       <h1>Hello World</h1>
+      <input type="text" onClick={inputbutton} />
     </div>
   );
 }
