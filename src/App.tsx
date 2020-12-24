56a723dc618994d9b47b687eadf848c97bbec6bc
556 <68994528+556-mizusawa@users.noreply.github.com>
2020-12-24 13:27:16 +0900

    learn State

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,13 @@
 import React from 'react';
+import Counter from './counter';
 
 interface UserProps {
   name?: string;
   age?: number;
 }
 
-const App: React.FunctionComponent<UserProps> = () => {
-  const profiles: {
-    name: string;
-    age?: number;
-  }[] = [
-    { name: 'Taro', age: 10 },
-    { name: 'Hanako', age: 5 },
-    { name: 'Noname' },
-  ];
-
-  return (
-    <div>
-      {profiles.map((profile, index) => {
-        return <User name={profile.name} age={profile.age} key={index} />;
-      })}
-    </div>
-  );
-};
-
-const User = (props: UserProps) => {
-  return (
-    <div>
-      <p>Hi, I am {props.name},</p>
-      <p>and {props.age} years old</p>
-    </div>
-  );
-};
-
-User.defaultProps = {
-  age: 'Secrets',
+const App: React.FC<UserProps> = () => {
+  return <Counter />;
 };
 
 export default App;
---
+++ src/counter.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+const Counter: React.FC<{}> = () => {
+  const [value, setValue] = useState<number>(0);
+
+  const incriment = () => {
+    setValue((prevSrate) => prevSrate + 1);
+  };
+
+  const decrement = () => {
+    setValue((prevSrate) => prevSrate - 1);
+  };
+
+  const renderTimes = useRef<number>(0);
+  useEffect(() => {
+    renderTimes.current = renderTimes.current + 1;
+  });
+
+  return (
+    <div>
+      <div>value: {value}</div>
+      <button onClick={incriment}>+1</button>
+      <button onClick={decrement}>-1</button>
+      <div>This component was re-rendered {renderTimes.current} times! </div>
+    </div>
+  );
+};
+
+export default Counter;
