1bb6b015b7c2ab5fa8b8822cf6b30417ee8ed2dd
556 <68994528+556-mizusawa@users.noreply.github.com>
2020-12-24 10:47:22 +0900

    create function component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 
-function App() {
-  const inputbutton = (): void => console.log('hello');
+// function App() {
+//   const inputbutton = (): void => console.log('hello');
 
+//   return (
+//     <div>
+//       <h1>Hello World</h1>
+//       <input type="text" onClick={inputbutton} />
+//     </div>
+//   );
+// }
+
+const App: () => JSX.Element = () => {
   return (
     <div>
-      <h1>Hello World</h1>
-      <input type="text" onClick={inputbutton} />
+      <Cat />
     </div>
   );
-}
+};
+
+const Cat: () => JSX.Element = () => <div>Meow!</div>;
 
 export default App;
