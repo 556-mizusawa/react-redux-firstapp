32fb5ba7ad3a08e1148a564ee7a10d8899d669ab
556 <68994528+556-mizusawa@users.noreply.github.com>
2020-12-24 12:23:27 +0900

    learn props of  component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
 
-// function App() {
-//   const inputbutton = (): void => console.log('hello');
+interface AppProps {
+  name?: string;
+  age?: number;
+}
 
-//   return (
-//     <div>
-//       <h1>Hello World</h1>
-//       <input type="text" onClick={inputbutton} />
-//     </div>
-//   );
-// }
+const App: React.FunctionComponent<AppProps> = () => {
+  const profiles: {
+    name: string;
+    age?: number;
+  }[] = [
+    { name: 'Taro', age: 10 },
+    { name: 'Hanako', age: 5 },
+    { name: 'Noname' },
+  ];
 
-const App: () => JSX.Element = () => {
   return (
     <div>
-      <Cat />
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />;
+      })}
     </div>
   );
 };
 
-const Cat: () => JSX.Element = () => <div>Meow!</div>;
+const User = (props: AppProps) => {
+  return (
+    <div>
+      <p>Hi, I am {props.name},</p>
+      <p>and {props.age} years old</p>
+    </div>
+  );
+};
+
+User.defaultProps = {
+  age: 1,
+};
 
 export default App;
